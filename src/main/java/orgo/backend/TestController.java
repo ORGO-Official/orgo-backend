b295adcd710e208bea74a209407c12b51c1d0e91
이한솔 <hansol8701@naver.com>
2023-07-16 12:42:03 +0900

    setting: RestDocs 설정
    - RestDocs 동작 확인을 위해 TestController에 임시 API를 생성함.
    - build.gradle에 RestDocs 설정을 추가하였음(번호 부여함)
    - TestForTest에서 임시 API에 대한 통합 테스트를 진행한 후, Docs 구성에 필요한 스니펫을 생성함.
    - src/docs/asciidoc/index.adoc 에서 스니펫을 이용해 문서를 구성함.
    - build나 bootJar을 수행하면, index.adoc의 내용을 바탕으로 src/main/resources/static/docs/index.html을 생성함 (어플리케이션 실행 후,  http://localhost:8080/docs/index.html 로 접속해 확인 가능)

---
+++ src/main/java/orgo/backend/TestController.java
@@ -0,0 +1,32 @@
+package orgo.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @PostMapping("/test")
+    public ResponseEntity<TestResponse> test(@RequestBody TestRequest requestDto) {
+        return new ResponseEntity<>(new TestResponse(requestDto.getField()), HttpStatus.OK);
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TestRequest {
+        String field;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class TestResponse {
+        String field;
+    }
+}
--- src/test/java/orgo/backend/BackendApplicationTests.java
+++ src/test/java/orgo/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package orgo.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"inttest", "test"})
 class BackendApplicationTests {
 
 	@Test
