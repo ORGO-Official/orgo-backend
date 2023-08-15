abab73d0111ac71fb82bd4b2f056f5e8e5c4a640
HANSOL <45627010+lhs8701@users.noreply.github.com>
2023-08-16 01:33:19 +0900

    [Hotfix/19] 유저 엔티티 필드 수정 및 로그인 이슈 해결 (#20)
    * setting: 네이버 클라이언트 아이디와 클라이언트 시크릿 등록
    
    * refactor: User 엔티티에서 생년월일, 연령 필드 제거

--- src/main/java/orgo/backend/domain/_2user/domain/Gender.java
+++ src/main/java/orgo/backend/domain/_2user/domain/Gender.java
@@ -5,16 +5,21 @@ import lombok.Getter;
 import orgo.backend.domain._1auth.domain.LoginType;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 @AllArgsConstructor
 @Getter
 public enum Gender {
     FEMALE(Map.of(LoginType.NAVER, "F", LoginType.KAKAO, "female")),
-    MALE(Map.of(LoginType.NAVER, "M", LoginType.KAKAO, "male"));
+    MALE(Map.of(LoginType.NAVER, "M", LoginType.KAKAO, "male")),
+    UNDEFINED(Map.of(LoginType.NAVER, "None", LoginType.KAKAO, "None"));
 
     private final Map<LoginType, String> map;
     public static Gender findBy(LoginType loginType, String name) {
+        if (Objects.isNull(name)){
+            return UNDEFINED;
+        }
         return Stream.of(values())
                 .filter(e -> e.getMap().get(loginType).equals(name))
                 .findFirst().orElseThrow(RuntimeException::new);
