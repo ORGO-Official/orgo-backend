89eed415e52d79436795d60c14e5f7a75bc94063
devMuscle <hongsbien@naver.com>
2023-08-20 17:44:26 +0900

    style: import 변경

--- src/main/java/orgo/backend/domain/_4climbingRecord/domain/ClimbingRecord.java
+++ src/main/java/orgo/backend/domain/_4climbingRecord/domain/ClimbingRecord.java
@@ -1,10 +1,7 @@
 package orgo.backend.domain._4climbingRecord.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import orgo.backend.domain._2user.domain.User;
 import orgo.backend.domain._3mountain.domain.Mountain;
 import orgo.backend.global.config.jpa.BaseTimeEntity;
