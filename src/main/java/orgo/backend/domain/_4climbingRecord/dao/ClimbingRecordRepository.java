b9085187f38432ec2b390a4cdc2af89f8235ef87
devMuscle <hongsbien@naver.com>
2023-08-24 14:23:47 +0900

    feat: findbyuserId 메서드 추가

--- src/main/java/orgo/backend/domain/_4climbingRecord/dao/ClimbingRecordRepository.java
+++ src/main/java/orgo/backend/domain/_4climbingRecord/dao/ClimbingRecordRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import orgo.backend.domain._4climbingRecord.domain.ClimbingRecord;
 
 public interface ClimbingRecordRepository extends JpaRepository<ClimbingRecord, Long> {
+    ClimbingRecord findByUserId(Long userId);
 }
