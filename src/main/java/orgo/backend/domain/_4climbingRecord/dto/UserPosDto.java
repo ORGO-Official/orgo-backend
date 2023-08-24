e0e7e4b9f56ed91add4aaf5a63237602ec6e4715
devMuscle <hongsbien@naver.com>
2023-08-24 19:18:09 +0900

    feat: 생성자 추가

--- src/main/java/orgo/backend/domain/_4climbingRecord/dto/UserPosDto.java
+++ src/main/java/orgo/backend/domain/_4climbingRecord/dto/UserPosDto.java
@@ -3,12 +3,14 @@ package orgo.backend.domain._4climbingRecord.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserPosDto {
     private Long mountainId;
     private double latitude;
