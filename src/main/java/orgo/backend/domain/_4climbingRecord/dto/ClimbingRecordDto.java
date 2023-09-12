60e092fabc749b803c26d1d94eb09fc1c20dca8d
devMuscle <hongsbien@naver.com>
2023-09-12 09:46:20 +0900

    fix: resolve conflict

--- src/main/java/orgo/backend/domain/_4climbingRecord/dto/ClimbingRecordDto.java
+++ src/main/java/orgo/backend/domain/_4climbingRecord/dto/ClimbingRecordDto.java
@@ -15,19 +15,18 @@ public class ClimbingRecordDto {
     private Long mountainId;
     private String mountainName;
     private LocalDateTime date;
-<<<<<<< HEAD
+
     private double altitude;
     private Long climbingOrder;
 
     public void setClimbingOrder(Long climbingOrder) {
         this.climbingOrder = climbingOrder;
-=======
+    }
 
     public ClimbingRecordDto(ClimbingRecord climbingRecord){
         this.id = climbingRecord.getId();
         this.mountainId = climbingRecord.getMountain().getId();
         this.mountainName = climbingRecord.getMountain().getName();
         this.date = climbingRecord.getDate();
->>>>>>> c14b908872a635fadddfaed06ec90e3b2b25e1f0
     }
 }
