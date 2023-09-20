738d29f9ea11224e0b8e9251498d380e54b19845
hansol <hansol8701@naver.com>
2023-09-21 03:56:56 +0900

    :white_check_mark: 보유 뱃지 목록 조회 통합 테스트

---
+++ src/test/java/orgo/backend/integrationtest/BadgeApiTest.java
@@ -0,0 +1,42 @@
+package orgo.backend.integrationtest;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import orgo.backend.domain._5badge.dto.BadgeDto;
+import orgo.backend.global.constant.Header;
+import orgo.backend.setting.IntegrationTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class BadgeApiTest extends IntegrationTest {
+
+    private final static String GET_ACQUIRED_BADGES = "/api/badges/acquired";
+
+    @Test
+    @DisplayName("보유한 뱃지 목록을 조회한다.")
+    void getAcquiredBadges(){
+        //given
+
+        //when
+        ExtractableResponse<Response> response = RestAssured
+                .given().log().all()
+                .header(Header.AUTH, getAccessToken())
+                .when().get(GET_ACQUIRED_BADGES)
+                .then().log().all()
+                .extract();
+
+        //then
+        JsonPath jsonPath = response.jsonPath();
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(jsonPath.getList("$", BadgeDto.Acquired.class)).isEmpty()
+        );
+    }
+}
