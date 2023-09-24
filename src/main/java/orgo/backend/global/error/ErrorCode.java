842459d2c42f533fad614472e1c6614e57e150e9
hansol <hansol8701@naver.com>
2023-09-24 22:55:55 +0900

    :recycle: 토큰 만료 시에 401을, 유효하지 않을 때는 400 상태 코드를 리턴하도록 변경

--- src/main/java/orgo/backend/global/error/ErrorCode.java
+++ src/main/java/orgo/backend/global/error/ErrorCode.java
@@ -15,10 +15,10 @@ public enum ErrorCode {
     SOCIAL_TOKEN_EXPIRED("0003", "소셜 토큰이 만료되었습니다. 재발급해주세요.", HttpStatus.BAD_REQUEST),
     CONNECTION_FAILED("0004", "외부 API 통신 중에 오류가 발생헀습니다. ", HttpStatus.BAD_REQUEST),
     RESOURCE_NOT_FOUND("0005", "해당 리소스를 찾을 수 없습니다. ", HttpStatus.NOT_FOUND),
-    INVALID_JWT("0006", "유효하지 않은 토큰입니다.", HttpStatus.UNAUTHORIZED),
+    INVALID_JWT("0006", "유효하지 않은 토큰입니다.", HttpStatus.BAD_REQUEST),
     EXPIRED_JWT("0007", "만료된 토큰입니다.", HttpStatus.UNAUTHORIZED),
-    UNSUPPORTED_JWT("0008", "지원하지 않는 토큰 형식입니다.", HttpStatus.UNAUTHORIZED),
-    WRONG_SIGNATURE_JWT("0009", "토큰의 시그니처가 잘못되었습니다.", HttpStatus.UNAUTHORIZED),
+    UNSUPPORTED_JWT("0008", "지원하지 않는 토큰 형식입니다.", HttpStatus.BAD_REQUEST),
+    WRONG_SIGNATURE_JWT("0009", "토큰의 시그니처가 잘못되었습니다.", HttpStatus.BAD_REQUEST),
     MOUNTAIN_NOT_FOUND("0010", "해당 산을 조회할 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
