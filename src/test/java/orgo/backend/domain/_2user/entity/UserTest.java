8fab49aef25a2f5b02679666baaf17ebe51bf2ed
hansol <hansol8701@naver.com>
2023-09-25 15:53:13 +0900

    :white_check_mark: 사용자가 특정 산을 등반한 횟수를 조회하는 로직을 검증

--- src/test/java/orgo/backend/domain/_2user/entity/UserTest.java
+++ src/test/java/orgo/backend/domain/_2user/entity/UserTest.java
@@ -1,12 +1,20 @@
 package orgo.backend.domain._2user.entity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import orgo.backend.domain._3mountain.entity.Mountain;
+import orgo.backend.domain._4climbingRecord.entity.ClimbingRecord;
 import orgo.backend.domain._5badge.entity.Badge;
+import orgo.backend.domain._5badge.entity.RecordCountBadge;
 import orgo.backend.domain._5badge.entity.acquisition.Acquisition;
 import orgo.backend.setting.MockEntityFactory;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
 class UserTest {
 
     @Test
@@ -24,4 +32,34 @@ class UserTest {
         //then
         Assertions.assertThat(result).isTrue();
     }
+
+
+    @Test
+    @DisplayName("사용자가 특정 산을 등반한 횟수를 조회한다.")
+    void getCountOfMountainClimbed(){
+        //given
+        User user = MockEntityFactory.mockUser(1L);
+        Mountain mountain = MockEntityFactory.mockMountain(1L, MockEntityFactory.mockPeak(1L));
+        ClimbingRecord record1 = ClimbingRecord.builder()
+                .id(1L)
+                .user(user)
+                .mountain(mountain)
+                .date(LocalDateTime.of(2023, 9, 1, 15, 23, 44))
+                .build();
+
+        ClimbingRecord record2 = ClimbingRecord.builder()
+                .id(2L)
+                .user(user)
+                .mountain(mountain)
+                .date(LocalDateTime.of(2023, 9, 2, 15, 23, 44))
+                .build();
+
+        //when
+        long result = user.countOfMountainClimbed(mountain.getName());
+
+        //then
+        Assertions.assertThat(result).isEqualTo(2L);
+    }
+
+
 }
