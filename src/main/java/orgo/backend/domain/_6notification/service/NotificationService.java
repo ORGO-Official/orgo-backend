f0a8ae746d4c8353dacf580f5578141514118abb
hansol <hansol8701@naver.com>
2023-09-26 16:58:40 +0900

    :sparkles: FCM을 활용패 푸시 메시지를 보내는 기능 구현

---
+++ src/main/java/orgo/backend/domain/_6notification/service/NotificationService.java
@@ -0,0 +1,82 @@
+package orgo.backend.domain._6notification.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class NotificationService {
+    @Value("${fcm.key.path}")
+    private String FCM_PRIVATE_KEY_PATH;
+
+    @Value("${fcm.key.scope}")
+    private String FIRE_BASE_SCOPE;
+
+    // fcm 기본 설정 진행
+    @PostConstruct
+    public void init() {
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials
+                            .fromStream(new ClassPathResource(FCM_PRIVATE_KEY_PATH).getInputStream())
+                            .createScoped(List.of(FIRE_BASE_SCOPE)))
+                    .build();
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("Firebase 서비스 초기화 성공");
+            }
+        } catch (IOException e) {
+            log.error("Firebase 서비스 초기화 중 오류 발생 : " + e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static Message notificationMessage(String token, String title, String body) {
+        Notification notification = Notification.builder()
+                .setTitle(title)
+                .setBody(body)
+                .build();
+        return Message.builder()
+                .setNotification(notification)
+                .setToken(token)
+                .build();
+    }
+
+    public static Message dataMessage(String token, Map<String, String> content) {
+        return Message.builder()
+                .putAllData(content)
+                .setToken(token)
+                .build();
+    }
+
+    public void sendMessage(Message message) {
+        try {
+            FirebaseMessaging.getInstance().send(message);
+        } catch (FirebaseMessagingException e) {
+            log.error("메시지 전송에 실패했습니다. : {}", e.getMessage());
+            throw new RuntimeException();
+        }
+    }
+
+    public void sendAllMessages(List<Message> messages) {
+        try {
+            FirebaseMessaging.getInstance().sendAll(messages);
+        } catch (FirebaseMessagingException e) {
+            log.error("메시지 전송에 실패했습니다. : {}", e.getMessage());
+        }
+    }
+}
