5907fb2137c0f27bf108fac6b8ae6654535a7428
Ji Pyo Hong <91146369+devMuscle@users.noreply.github.com>
2023-10-08 17:13:38 +0900

    [Refactor] 완등 기록 관련 코드 리팩토링 및 버그 픽스 (#65)
    * setting: gitignoreã에 test 환ê설정 파일 추ê가
    
    * setting: 설정 파일 수정
    
    * refactor: api의 url 수정
    
    * refactor: 일반 exException에서 UserNotFoundException으로 변경
    
    * refactor: Mapstruct를 이용한 코드 간소화
    
    * remove ignored file
    
    * test: 변경ëì 완드등 기록 API 반환형 변경에 따른 테스트 코드 수정
    
    * frefactor: ì완등 기록 조회 API 반환형 추가
    
    * fix: resolve conflict
    
    * test: API 반환형 변경으로 인한 통합테스트 코드 변경
    
    * refactor: 불필요한 javadoc 제거
    
    * refactor: 거리 구하는 로직을 담당할 class 생성
    
    * refactor: ㅅtest시 발생하는 오류 수정
    
    * feat: 범위안 계싼산 로직 추가
    
    * refactor: 거리 계산 로직 분리 , ãearly return 적ã용, 완등 기록 정렬 ì내림차순으로 변경
    
    * test: 거리 계싼산 로직 분리로 인한 코드 수정 및 실패 경우 테스트 추ê가

--- src/main/java/orgo/backend/domain/_4climbingRecord/service/ClimbingRecordService.java
+++ src/main/java/orgo/backend/domain/_4climbingRecord/service/ClimbingRecordService.java
@@ -7,6 +7,8 @@ import orgo.backend.domain._2user.repository.UserRepository;
 import orgo.backend.domain._2user.entity.User;
 import orgo.backend.domain._3mountain.repository.MountainRepository;
 import orgo.backend.domain._3mountain.entity.Mountain;
+import orgo.backend.domain._4climbingRecord.dto.Position;
+import orgo.backend.domain._4climbingRecord.mapper.PositionMapper;
 import orgo.backend.domain._4climbingRecord.repository.ClimbingRecordRepository;
 import orgo.backend.domain._4climbingRecord.entity.ClimbingRecord;
 import orgo.backend.domain._4climbingRecord.dto.ClimbingRecordDto;
@@ -28,13 +30,6 @@ public class ClimbingRecordService {
     private final UserRepository userRepository;
     private final RecordBadgeFactory recordBadgeFactory;
 
-    /**
-     * 등산 완등 인증 요청을 처리합니다.
-     * 이후 발급 가능한 뱃지가 있으면, 발급합니다.
-     *
-     * @param userId     사용자 Id
-     * @param userPosDto 사용자의 위치 정보
-     */
     @Transactional
     public void registerClimbingRecord(Long userId, UserPosDto userPosDto) {
         if (isTop(userPosDto)) {
@@ -47,17 +42,13 @@ public class ClimbingRecordService {
 
             climbingRecordRepository.save(climbingRecord);
             recordBadgeFactory.issueAvailableBadges(user);
-        } else {
-            throw new RuntimeException();
+
+            return;
         }
+
+        throw new RuntimeException();
     }
 
-    /**
-     * 사용자의 모든 완등 기록을 조회합니다.
-     *
-     * @param userId 사용자 Id
-     * @return 사용자의 완등 기록
-     */
     public MyClimbingRecordDto viewMyClimbingRecords(Long userId) {
         User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
 
@@ -90,14 +81,14 @@ public class ClimbingRecordService {
             }
         }
 
-        // TODO : 다시 시간순 정렬
+        // TODO : 다시 시간순 내림차순 정렬
         List<ClimbingRecordDto> indexedClimbingRecordDtoList = new ArrayList<>();
 
         for (List<ClimbingRecordDto> records : climbingRecordDtoMap.values()) {
             indexedClimbingRecordDtoList.addAll(records);
         }
 
-        indexedClimbingRecordDtoList.sort(Comparator.comparing(ClimbingRecordDto::getDate));
+        indexedClimbingRecordDtoList.sort(Comparator.comparing(ClimbingRecordDto::getDate).reversed());
 
         MyClimbingRecordDto myClimbingRecordDto = MyClimbingRecordDto.builder()
                 .climbingRecordDtoList(climbingRecordDtoList)
@@ -108,52 +99,14 @@ public class ClimbingRecordService {
         return myClimbingRecordDto;
     }
 
-    /**
-     * 사용자가 완등 인증이 가능한 위치인지 판단합니다.
-     *
-     * @param userPosDto 사용자의 위치 정보
-     * @return 완등 가능 위치인지 여부
-     */
     public boolean isTop(UserPosDto userPosDto) {
-        double distanceDiffRange = 500;
-        double altitudeDiffRange = 50;
+        final double distanceRange = 500;
+        final double altitudeRange = 50;
         Mountain mountain = mountainRepository.findById(userPosDto.getMountainId()).orElseThrow();
+        Position userPosition = PositionMapper.INSTANCE.toPostion(userPosDto);
+        Position mountainPosition = PositionMapper.INSTANCE.toPostion(mountain);
 
-        double distanceDiff = calDistance(userPosDto.getLatitude(), userPosDto.getLongitude(),
-                mountain.getLocation().getLatitude(), mountain.getLocation().getLongitude());
-
-        if (distanceDiff > distanceDiffRange || Math.abs(userPosDto.getAltitude() - mountain.getLocation().getAltitude()) > altitudeDiffRange) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * Haversine공식을 기반으로 두 장소간의 거리를 계산합니다.
-     *
-     * @param latitude1  위도1
-     * @param longitude1 경도1
-     * @param latitude2  위도1
-     * @param longitude2 경도1
-     * @return 두 거리간의 거리
-     */
-    public double calDistance(double latitude1, double longitude1, double latitude2, double longitude2) {
-        double RADIUS_OF_EARTH_KM = 6371.0;
-        double latitudeRad1 = Math.toRadians(latitude1);
-        double longitudeRad1 = Math.toRadians(longitude1);
-        double latitudeRad2 = Math.toRadians(latitude2);
-        double LongitudeRad2 = Math.toRadians(longitude2);
-
-        double latitudeDiff = latitudeRad2 - latitudeRad1;
-        double LongitudeDiff = LongitudeRad2 - longitudeRad1;
-
-        double a = Math.sin(latitudeDiff / 2) * Math.sin(latitudeDiff / 2) +
-                Math.cos(latitudeRad1) * Math.cos(latitudeRad2) *
-                        Math.sin(LongitudeDiff / 2) * Math.sin(LongitudeDiff / 2);
-
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-        return RADIUS_OF_EARTH_KM * c;
+        return mountainPosition.isDistanceWithinRange(userPosition, distanceRange) &&
+                mountainPosition.isAltitudeWithinRange(userPosition, altitudeRange);
     }
 }
